4fb36db246a835db723afa94561500352d6ca3a5
yasin tuna <44206859+yasin570@users.noreply.github.com>
2023-09-21 20:12:50 +0300

    examples

---
+++ examples.ino
@@ -0,0 +1,17 @@
+#define RemoteSender_h
+
+
+String data = "A1,F1,T5";
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void send(String data);
